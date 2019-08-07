ead640ca81931cecfc097bcb53c1b22437dd622a
edjunma <junwenma@buffalo.edu>
2019-08-07 17:53:34 -0400

    setting up backend

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+// Load env
+dotenv.config({ path: './config.env' });
+
+const app = express();
+
+app.listen(5000, () => {
+	console.log('App listening on port 5000');
+});
